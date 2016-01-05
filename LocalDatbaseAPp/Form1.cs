287aa9bf985de30608569b09c874804bade3003c
ashm01 <a.moore2@uea.ac.uk>
2016-01-05 16:35:08 +0000

    First Commit

---
+++ LocalDatbaseAPp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocalDatbaseAPp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void employeeInfoBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.employeeInfoBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.employeeDataSet1);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'employeeDataSet1.EmployeeInfo' table. You can move, or remove it, as needed.
+            this.employeeInfoTableAdapter.Fill(this.employeeDataSet1.EmployeeInfo);
+
+        }
+
+        private void addButtton_Click(object sender, EventArgs e)
+        {
+            this.employeeInfoBindingSource.AddNew();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.employeeInfoBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.employeeDataSet1);
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            this.employeeInfoBindingSource.RemoveCurrent();
+        }
+    }
+}
